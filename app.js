1af756195f14f4254a53dacb0bb7b85af21bb719
Wai Yan Min <waiyanmin.lane@gmail.com>
2024-09-05 00:18:56 -0700

    exercise callbacks and error handling

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const rect =require('./rectangle.js');
 function solveRect(l,w){
     console.log(`The give rectangle with the value of ${l} length and ${w} width`);
-    if(l<=0 || w<=0){
-        console.log (`Rectangle dimenssions must be greater than zero, Recieved: ${l}, ${w}`);
+    rect(l,w,(err,rectangle)=>{
+        if(err){
+            console.log('Error:',err.message);
+        }
+        else{
+            console.log(`The area of the rectangle is ${rectangle.area()}`);
+            console.log(`The parimeter of the rectangle is ${rectangle.parimeter()}`);
 
-    }else {
-        console.log(`The area of rectangle is : ${rect.area(l,w)} \n The parameter of rectangle is : ${rect.parameter(l,w)}`);
+        }
 
-    }
+    });
+    console.log('This statement is log after the call to rect()');
 }
 solveRect(1,2);
 solveRect(2,3);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,11 @@
-exports.parameter = (x,y)=> 2*(x+y);
-exports.area =(x,y)=> x*y;
+module.exports=(x,y,callback)=>{
+    if(x<=0 || y<=0){
+        callback(new Error(`Rectangle dimenssions must be greater than zero, Recieved: ${x}, ${y}`));
+    }else {
+        setTimeout(()=>
+            callback(null,{
+                parimeter:()=> 2* (x+y),
+                area :()=> x*y
+            }),2000);
+    }
+};
